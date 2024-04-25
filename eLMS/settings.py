0d179d3ad1052977b66b64423019292927d60195
sravan <sravankumar7384@gmail.com>
2024-04-25 19:58:56 +0530

    initial commit

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -169,10 +169,6 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # settings.py
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sravankumar7384@gmail.com'
-EMAIL_HOST_PASSWORD = 'sravanKumar@710'
+
+
 
